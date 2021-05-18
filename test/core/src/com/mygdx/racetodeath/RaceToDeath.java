30f16d945c12ae8d4358aaf7432a3e5655f00bc2
giahuy-204 <giahuy.dng@gmail.com>
2021-05-18 12:47:25 +0700

    adjust background

--- test/core/src/com/mygdx/racetodeath/RaceToDeath.java
+++ test/core/src/com/mygdx/racetodeath/RaceToDeath.java
@@ -1,31 +1,26 @@
 package com.mygdx.racetodeath;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class RaceToDeath extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture background;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("start1.png");
+		background = new Texture("start1.png");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		batch.end();
 	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+
 }
