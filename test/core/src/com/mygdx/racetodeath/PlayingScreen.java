231c149e7538d5ec59af184c1443c6a4a97fa608
giahuy-204 <giahuy.dng@gmail.com>
2021-05-18 14:40:31 +0700

    adding assets and scrolling screen

---
+++ test/core/src/com/mygdx/racetodeath/PlayingScreen.java
@@ -0,0 +1,81 @@
+package com.mygdx.racetodeath;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+import javax.xml.soap.Text;
+
+public class PlayingScreen implements Screen {
+
+    private Camera camera;
+    private Viewport viewport;
+
+    private SpriteBatch batch;
+    private Texture background;
+
+    private int backgroundOffset;
+
+    private final int WORLD_WIDTH = 72;
+    private final int WORLD_HEIGHT = 128;
+
+    PlayingScreen() {
+        camera = new OrthographicCamera();
+        viewport = new StretchViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
+
+        background = new Texture("start1.png");
+        backgroundOffset = 0;
+
+        batch = new SpriteBatch();
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        batch.begin();
+
+        backgroundOffset++;
+        if (backgroundOffset % WORLD_HEIGHT == 0) {
+            backgroundOffset = 0;
+        }
+
+        batch.draw(background, 0, -backgroundOffset, WORLD_WIDTH, WORLD_HEIGHT);
+        batch.draw(background, 0, -backgroundOffset + WORLD_HEIGHT, WORLD_WIDTH, WORLD_HEIGHT);
+
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height, true);
+        batch.setProjectionMatrix(camera.combined);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
--- test/core/src/com/mygdx/racetodeath/RaceToDeath.java
+++ test/core/src/com/mygdx/racetodeath/RaceToDeath.java
@@ -1,26 +1,33 @@
 package com.mygdx.racetodeath;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Game;
 
-public class RaceToDeath extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture background;
-	
+public class RaceToDeath extends Game {
+
+	PlayingScreen playingScreen;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		background = new Texture("start1.png");
+		playingScreen = new PlayingScreen();
+		setScreen(playingScreen);
 	}
 
 	@Override
 	public void render () {
-		batch.begin();
-		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		batch.end();
+		super.render();
 	}
+
+	@Override
+	public void dispose() {
+		playingScreen.dispose();
+	}
+
+
+	@Override
+	public void resize(int width, int height) {
+		playingScreen.resize(width, height);
+	}
+
 
 }
